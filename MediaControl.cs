bbb10d156a0bc2ffa6d3ddb1107221efedb0030a
tumuyan <tumuyan@gmail.com>
2026-05-23 17:26:16 +0800

    bump to 1.0.6

--- MediaControl.cs
+++ MediaControl.cs
@@ -68,7 +68,7 @@ namespace MusicBeePlugin
             about.Type = PluginType.General;
             about.VersionMajor = 1;
             about.VersionMinor = 0;
-            about.Revision = 5;
+            about.Revision = 6;
             about.MinInterfaceVersion = MinInterfaceVersion;
             about.MinApiRevision = MinApiRevision;
             about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
